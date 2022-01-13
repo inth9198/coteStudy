0fc8222ba1ee4427a9a6de58bb8d2ffa907608d9
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2022-01-14 01:29:06 +0900

    Delete 김민기/chapter03 directory

--- 김민기/chapter03/숫자 카드 게임.py
+++
@@ -1,9 +0,0 @@
-N, M = map(int, input().split())
-
-result = 0
-for i in range(N):
-    arr = list(map(int, input().split()))
-    min_Value = min(arr)
-    result = max(result, min_Value)
-
-print(result)
