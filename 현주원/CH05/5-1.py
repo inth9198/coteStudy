af8944b5085f1707747aed1e3bee4f6bd1c0c4e0
jinny1904 <73792330+jinny1904@users.noreply.github.com>
2022-01-14 14:53:31 +0900

    Create 5-1.py

---
+++ 현주원/CH05/5-1.py
@@ -0,0 +1,16 @@
+#스택 : 선입후출구조(먼저 넣은게 가장 나중에 나간다)
+
+stack=[] 
+
+#삽입(5)-삽입(2)-삽입(3)-삽입(7)-삭제()-삽입(1)-삽입(4)-삭제()
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)        #최하단 원소부터 출력 [5,2,3,1]
+print(stack[::1])   #최상단 원소부터 출력 [1,3,2,5]
