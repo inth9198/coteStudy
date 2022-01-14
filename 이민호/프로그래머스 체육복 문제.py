82c0d114886352906e5f5305bc176fbadf4e96da
inth9198 <82989054+inth9198@users.noreply.github.com>
2022-01-14 13:58:31 +0900

    Update 프로그래머스 체육복 문제.py

--- 이민호/프로그래머스 체육복 문제.py
+++ 이민호/프로그래머스 체육복 문제.py
@@ -6,14 +6,14 @@ def solution(n, lost, reserve):
         std.append(1)
     for i in lost:
         std[i] = 0
-    lost.sort() #정렬을 꼭해야해 ㅠㅠㅠ
+    lost.sort() #정렬을 꼭 해야 합니다 정렬되지 않은 테케가 들어오기도 합니다
     reserve.sort()#
-    for i in reserve:  #후에 친구가 먼저 자기껄 빌려줄수가 있음(지도 없으면서)
+    for i in reserve:  #후에 친구가 먼저 자기껄 빌려줄수가 있습니다.
         if std[i] == 0:
             std[i] = 1
         elif i >= 2 and std[i - 1] == 0:
             std[i - 1] = 1
-        elif i <= n - 1 and std[i + 1] == 0: #n으로 안쓰고 숫자로 썻네..
+        elif i <= n - 1 and std[i + 1] == 0:
             if (i + 1 in reserve):
                 continue
             std[i + 1] = 1
