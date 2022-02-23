70fa15e194b7bd7bd0a9363319d669418bf03df0
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2022-02-24 00:16:53 +0900

    Chapter09

---
+++ 김민기/Chapter09/전보.py
@@ -0,0 +1,47 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n, m, c = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+distance = [INF] * (n + 1)
+
+def dijstra(c):
+    q = []
+    heapq.heappush(q, (0, c))
+    distance[c] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijstra(c)
+
+cnt = 0
+max_distance = 0
+for d in distance:
+    if d != INF:
+        cnt += 1
+        max_distance = max(max_distance, d)
+
+print(cnt - 1, max_distance)
+
+
+
+
+
+
+
+
